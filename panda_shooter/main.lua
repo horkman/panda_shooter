0aeaa5712438ed09335a33a8fc3c568b9f912384
Adam Horky <adam.horky06@gmail.com>
2023-04-06 08:37:38 -0500

    more

---
+++ panda_shooter/main.lua
@@ -0,0 +1,47 @@
+--
+--
+-- Yay
+--
+--
+local player
+local enemy
+
+function love.load()
+    Object = require "libraries.classic"
+    require "player"
+    require "enemy"
+    require "bullet"
+
+    player = Player()
+    enemy = Enemy()
+    listOfBullets = {}
+end
+
+function love.update(dt)
+    player:update(dt)
+    enemy:update(dt)
+
+    for i, v in ipairs(listOfBullets) do
+        v:update(dt)
+
+        -- Each bullets checks if there is collision with the enemy
+        v:checkCollision(enemy)
+
+        if v.dead then
+            table.remove(listOfBullets, i)
+        end
+    end
+end
+
+function love.draw()
+    player:draw()
+    enemy:draw()
+
+    for i, v in ipairs(listOfBullets) do
+        v:draw()
+    end
+end
+
+function love.keypressed(key)
+    player:keyPressed(key)
+end
